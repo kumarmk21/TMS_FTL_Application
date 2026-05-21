97d4fd79130f581c72cf5bf0c9cb12a99dd8b65e
kumarmk21 <112611303+kumarmk21@users.noreply.github.com>
2026-05-21 13:24:24 +0530

    Updated AddVendorModal.tsx

--- src/components/modals/AddVendorModal.tsx
+++ src/components/modals/AddVendorModal.tsx
@@ -294,6 +294,8 @@ export function AddVendorModal({ isOpen, onClose, onSuccess }: AddVendorModalPro
                 name="account_no"
                 value={formData.account_no}
                 onChange={handleInputChange}
+                autoComplete="off"
+                inputMode="text"
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
                 required
               />
--- src/components/modals/EditVendorModal.tsx
+++ src/components/modals/EditVendorModal.tsx
@@ -348,6 +348,8 @@ export function EditVendorModal({ isOpen, onClose, onSuccess, vendor }: EditVend
                 name="account_no"
                 value={formData.account_no}
                 onChange={handleInputChange}
+                autoComplete="off"
+                inputMode="text"
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500"
                 required
               />
